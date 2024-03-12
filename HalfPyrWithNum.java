8373cb9a55227802516aee594b72068395e62fcc
ssroy000 <161429550+ssroy000@users.noreply.github.com>
2024-03-12 22:13:27 +0530

    Update HalfPyrWithNum.java

--- HalfPyrWithNum.java
+++ HalfPyrWithNum.java
@@ -22,3 +22,28 @@ public class HalfPyrawithNum {
         }
     }
 }
+
+/*
+
+1
+22
+333
+4444
+55555
+
+*/
+import java.util.Scanner;
+
+public class HalfPyrawithNum {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n=sc.nextInt();
+        for (int i=1; i<=n; i++){
+            for (int j=1; j<=i; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
